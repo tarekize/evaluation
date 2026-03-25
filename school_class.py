47e01cdd4058ba4b673ba353d7f57129cc028ef7
tarekize <tarekize7@yahoo.com>
2026-03-25 13:20:08 +0100

    Etape 6: Ajout Matter2Iterator et Matter3Iterator

--- school_class.py
+++ school_class.py
@@ -12,6 +12,30 @@ class Matter1Iterator(Iterator):
         self.index += 1
         return student
 
+class Matter2Iterator(Iterator):
+    def __init__(self, students):
+        self.sorted_students = sorted(students, key=lambda x: x.subject2, reverse=True)
+        self.index = 0
+
+    def __next__(self):
+        if self.index >= len(self.sorted_students):
+            raise StopIteration
+        student = self.sorted_students[self.index]
+        self.index += 1
+        return student
+
+class Matter3Iterator(Iterator):
+    def __init__(self, students):
+        self.sorted_students = sorted(students, key=lambda x: x.subject3, reverse=True)
+        self.index = 0
+
+    def __next__(self):
+        if self.index >= len(self.sorted_students):
+            raise StopIteration
+        student = self.sorted_students[self.index]
+        self.index += 1
+        return student
+
 class Student:
     def __init__(self, name, subject1, subject2, subject3):
         self.name = name
@@ -33,6 +57,12 @@ class SchoolClass(Iterable):
     def __iter__(self):
         return Matter1Iterator(self.students)
 
+    def iter_matter_2(self):
+        return Matter2Iterator(self.students)
+
+    def iter_matter_3(self):
+        return Matter3Iterator(self.students)
+
 if __name__ == '__main__':
     school_class = SchoolClass()
     school_class.add_student(Student('J', 10, 12, 13))
@@ -42,3 +72,11 @@ if __name__ == '__main__':
     print("--- Parcours avec l'Itérateur (Matière 1) ---")
     for student in school_class:
         print(f"{student.name} : {student.subject1}")
+
+    print("\n--- Parcours avec l'Itérateur (Matière 2) ---")
+    for student in school_class.iter_matter_2():
+        print(f"{student.name} : {student.subject2}")
+
+    print("\n--- Parcours avec l'Itérateur (Matière 3) ---")
+    for student in school_class.iter_matter_3():
+        print(f"{student.name} : {student.subject3}")
