da34bd80d7df8f97c5a2b576eaddcc7724c7d183
tarekize <tarekize7@yahoo.com>
2026-03-25 13:03:29 +0100

    Etape 3: Ajout de la méthode rank_matter_1

--- school_class.py
+++ school_class.py
@@ -16,8 +16,14 @@ class SchoolClass:
     def add_student(self, student: Student):
         self.students.append(student)
 
+    def rank_matter_1(self):
+        print("--- Classement Matière 1 ---")
+        for s in sorted(self.students, key=lambda x: x.subject1, reverse=True):
+            print(f"{s.name} : {s.subject1}")
+
 if __name__ == '__main__':
     school_class = SchoolClass()
     school_class.add_student(Student('J', 10, 12, 13))
     school_class.add_student(Student('A', 8, 2, 17))
     school_class.add_student(Student('V', 9, 14, 14))
+    school_class.rank_matter_1()
